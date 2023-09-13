2f742727845a3635c419090642e1a2970155be33
Anton Ermak <Rayman.k26@gmail.com>
2023-09-14 01:56:33 +0700

    JPA test, CI.

--- bank/src/test/java/de/telran/bank/AccountCreationTest.java
+++ bank/src/test/java/de/telran/bank/AccountCreationTest.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,7 +20,8 @@ import java.util.UUID;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@Sql("/schema-cleanup.sql")
+@Sql("/schema.sql")
 public class AccountCreationTest {
 
     @Autowired
